7f263effd9d726f8eda7d14cc1f736d295733517
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-08-12 09:39:27 -0300

    lendo e somando dois números

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
-        Console.WriteLine("Primeiro valor: ");
-
+        Console.WriteLine("Primeiro valor:");
         float v1 = float.Parse(Console.ReadLine()!);
-        Console.WriteLine(v1);
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine()!);
+
+        var resultado = v1 + v2;
+
+        Console.WriteLine($"O resultado da soma é: {resultado}");
     }
 }
