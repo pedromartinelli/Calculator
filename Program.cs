647a6e633613dd356b78459f67d56fa15246f63f
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-08-12 10:52:48 -0300

    criando um método de subtracao

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Soma();
+        Subtracao();
     }
 
     static void Soma()
@@ -15,8 +15,25 @@ class Program
         Console.WriteLine("Segundo valor:");
         float v2 = float.Parse(Console.ReadLine()!);
 
-        var resultado = v1 + v2;
+        Console.WriteLine("");
 
+        var resultado = v1 + v2;
         Console.WriteLine($"O resultado da soma é: {resultado}");
+        Console.ReadKey();
+    }
+
+    static void Subtracao()
+    {
+        Console.Clear();
+        Console.WriteLine("Primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine()!);
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("");
+
+        var resultado = v1 - v2;
+        Console.WriteLine($"O resultado da subtração é: {resultado}");
+        Console.ReadKey();
     }
 }
